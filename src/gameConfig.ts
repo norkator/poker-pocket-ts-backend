727a636e367a6c698871c2587792ca6d964059c4
norkator <martin.kankaanranta@hotmail.com>
2024-12-14 18:23:39 +0200

    defining more games in game config

--- src/gameConfig.ts
+++ src/gameConfig.ts
@@ -7,7 +7,7 @@ export const gameConfig = {
       startingTables: 1, // default 4, how many tables to create at start
       startMoney: 10000,
       bot: {
-        botCounts: [4,3,2,4,2,2], // add count for each starting table assuming you want bots in every table
+        botCounts: [4, 3, 2, 4, 2, 2], // add count for each starting table assuming you want bots in every table
         turnTimes: [1000, 1500, 2000, 2500, 3000],
         minMoney: [
           50,     // Low bet game
@@ -50,7 +50,43 @@ export const gameConfig = {
         }
       ],
     },
-    fiveCardDraw: {},
-    blackJack: {}
+    fiveCardDraw: {
+      startingTables: 1,
+      startMoney: 10000,
+      bot: {
+        botCounts: [2, 2, 2, 2],
+        turnTimes: [1000, 1500, 2000, 2500, 3000],
+        minMoney: [
+          50,     // Low bet game
+          200,    // Medium bet game
+          2000    // High bet game
+        ],
+        betAmounts: [
+          [25, 35, 100, 500],         // Low bet game
+          [125, 150, 200, 250],       // Medium bet game
+          [1100, 1200, 1500, 2000]    // High bet game
+        ]
+      }
+    },
+    bottleSpin: {
+      startingTables: 1,
+      startMoney: 10000,
+      bot: {
+        botCounts: [1, 1],
+        turnTimes: [1000, 1500, 2000, 2500, 3000],
+        minMoney: [
+          50,     // Low bet game
+          200,    // Medium bet game
+          2000    // High bet game
+        ],
+        betAmounts: [
+          [25, 35, 100, 500],         // Low bet game
+          [125, 150, 200, 250],       // Medium bet game
+          [1100, 1200, 1500, 2000]    // High bet game
+        ]
+      }
+    },
+    blackJack: {},
+    dices: {}
   }
 };
