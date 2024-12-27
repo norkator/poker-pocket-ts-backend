79a6880e891c2271946ab3e723258609822503bd
norkator <martin.kankaanranta@hotmail.com>
2024-12-27 19:33:39 +0200

    add dotenv dependency

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,8 @@
 import {Sequelize} from 'sequelize-typescript';
 import logger from '../logger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
