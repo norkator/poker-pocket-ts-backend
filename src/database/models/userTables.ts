f55f51df009ddd64619899650becaa4c10f9e5e9
norkator <martin.kankaanranta@hotmail.com>
2025-01-08 17:56:31 +0200

    user table create and get queries and socket handling

--- src/database/models/userTables.ts
+++ src/database/models/userTables.ts
@@ -1,12 +1,21 @@
-import {AllowNull, AutoIncrement, Column, DataType, ForeignKey, Model, PrimaryKey, Table} from 'sequelize-typescript';
+import {
+  AllowNull,
+  AutoIncrement,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
 import {User} from './user';
+import {Game} from '../../types';
 
 @Table({
   tableName: 'userTables',
   timestamps: true,
 })
 export class UserTable extends Model {
-
   @PrimaryKey
   @AutoIncrement
   @Column(DataType.BIGINT)
@@ -24,4 +33,44 @@ export class UserTable extends Model {
   @Column(DataType.STRING)
   password!: string;
 
+  @AllowNull(false)
+  @Column({
+    type: DataType.ENUM('HOLDEM', 'FIVE_CARD_DRAW', 'BOTTLE_SPIN'),
+  })
+  game!: Game;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+    defaultValue: 0,
+  })
+  botCount!: number;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+    defaultValue: 0,
+  })
+  turnCountdown!: number;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+    defaultValue: 0,
+  })
+  minBet!: number;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+    defaultValue: 0,
+  })
+  afterRoundCountdown!: number;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.INTEGER,
+    defaultValue: 0,
+  })
+  discardAndDrawTimeout!: number;
 }
